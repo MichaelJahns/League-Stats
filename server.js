7d8410f63b5be91a2a5c9988929bbf80b082e817
MichaelJahns <jahnsmic001@gmail.com>
2019-01-10 15:43:57 -0800

    first api successfully saved

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const superagent = require(`superagent`)
 require(`dotenv`).config();
 
 const app = express();
-const PORT = 3000;
+const PORT = 4000;
 
 app.use(express.urlencoded({extended:true}));
 app.use(express.static('public'));
@@ -20,33 +20,27 @@ app.get('/', pageLoad)
 
 app.post('/results', sendPlayerPackage)
 
+let accountInfo; 
+
 async function sendPlayerPackage(request, response){
-  console.log('Step 1 initiated')
-  console.log(getAccountInfo(request, response));
-
-  // console.log('Step 2 intitiated')
-  // const query = new playerPackage(accountInfo)
-
-  // response.render('/results', {playerPackage: query})
-}
-
-function playerPackage(accountInfo){
-  this.summonerName = accountInfo.name
-  this.summmonerLVL = accountInfo.summonerLevel
+  console.log('Initialized')
+  let stepOne = await getAccountInfo(request, response);
+  console.log('step one complete', stepOne.body)
 }
 
 function pageLoad(request, response) {
   response.render('pages/index.ejs')
 }
 
-function getAccountInfo(request, response){
-  
+async function getAccountInfo(request, response){
   let url = `https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${request.body.summonerName}?api_key=${process.env.RIOT_API_KEY}`
-  
+  console.log(url)
 
-  superagent.get(url)
-    .then(result => accountInfo = result.body.summonerName)
-    .catch(function(err){
-      console.log(`Something went wrong in the Account Info API call`)
-    })
-};
+  return superagent.get(url)
+    // .then(result => {
+    //   console.log('from the API', result.body)
+    // })
+    // .catch(function(err){
+    //   console.log(`Something went wrong in the Account Info API call`)
+    // })
+  }
