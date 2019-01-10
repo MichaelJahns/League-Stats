e3db0b660ee15962cbfafa3c5731042eef811a6a
MichaelJahns <jahnsmic001@gmail.com>
2019-01-10 11:48:44 -0800

    Sets up groundwork for first api call, which will return the encrypted summoner ID for future API use.

--- server.js
+++ server.js
@@ -8,14 +8,45 @@ require(`dotenv`).config();
 
 const app = express();
 const PORT = 3000;
+
+app.use(express.urlencoded({extended:true}));
+app.use(express.static('public'));
 app.use(cors());
 
+app.set('view engine', 'ejs');
 app.listen(PORT, () => console.log(`Listening on Port ${PORT}`));
+
 app.get('/', pageLoad)
 
+app.post('/results', sendPlayerPackage)
+
+async function sendPlayerPackage(request, response){
+  console.log('Step 1 initiated')
+  console.log(getAccountInfo(request, response));
+
+  // console.log('Step 2 intitiated')
+  // const query = new playerPackage(accountInfo)
+
+  // response.render('/results', {playerPackage: query})
+}
+
+function playerPackage(accountInfo){
+  this.summonerName = accountInfo.name
+  this.summmonerLVL = accountInfo.summonerLevel
+}
+
 function pageLoad(request, response) {
-  let url = `https://api.nasa.gov/planetary/apod?api_key=${process.env.NASA_IOD_API_KEY}`
+  response.render('pages/index.ejs')
+}
+
+function getAccountInfo(request, response){
+  
+  let url = `https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${request.body.summonerName}?api_key=${process.env.RIOT_API_KEY}`
+  
 
   superagent.get(url)
-    .then(result => response.render('pages/', {heroImage: result.body.url}));
-}
+    .then(result => accountInfo = result.body.summonerName)
+    .catch(function(err){
+      console.log(`Something went wrong in the Account Info API call`)
+    })
+};
