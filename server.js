e28ce3da63c7997d85854de32f56bd8c9b88cda8
MichaelJahns <jahnsmic001@gmail.com>
2019-01-17 17:12:56 -0800

    successfully pulls summoner match history with encrypted ID.

--- server.js
+++ server.js
@@ -27,18 +27,38 @@ function pageLoad(request, response) {
 
 async function sendPlayerPackage(request, response){
   console.log('Initialized')
-  let stepOne = await getAccountInfo(request, response);
-  console.log('step one complete', stepOne.body)
-  let stepTwo = await getMatches(stepOne, request, response);
-  console.log('step two complete', stepTwo.body)
+  let champion = await getChampionJSON(request, response)
+  console.log(champion.body.data)
+  let accountDemo = await getAccountInfo(request, response);
+  // console.log('step one complete', stepOne.body)
+  let accountMatches = await getMatches(accountDemo, request, response);
+  // console.log('step two complete', stepTwo.body)
+  var playerPackage = new Summoner(accountDemo.body, accountMatches.body)
+  // console.log('step three complete', playerPackage.match)
+  response.render('pages/results', {playerPackage})
 }
 
+// function champion(input){
+// this.champion = 
+// }
+function Summoner(demograhic, matchHistory){
+this.summoner = demograhic.name;
+this.profileIconId = 908;
+this.summonerLevel = demograhic.summonerLevel;
+this.accountId = demograhic.accountId;
+this.match = matchHistory.matches;
+
+}
+function getChampionJSON(request, response){
+  let url = `http://ddragon.leagueoflegends.com/cdn/6.24.1/data/en_US/champion.json`
+  return superagent.get(url)
+}
 function getAccountInfo(request, response){
   let url = `https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${request.body.summonerName}?api_key=${process.env.RIOT_API_KEY}`
   return superagent.get(url)
 }
 
-function getMatches(stepOne, request, response){
-  let url = `https://na1.api.riotgames.com/lol/match/v4/matchlists/by-account/${stepOne.body.accountId}?api_key=${process.env.RIOT_API_KEY}`
+function getMatches(accountDemo, request, response){
+  let url = `https://na1.api.riotgames.com/lol/match/v4/matchlists/by-account/${accountDemo.body.accountId}?api_key=${process.env.RIOT_API_KEY}`
   return superagent.get(url)
 }
